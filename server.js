94b219c388bd2abfe6f28136cb0b8ed202a9ea3b
Shovonx47 <shovon@lamptechs.com>
2025-01-25 01:40:42 -0800

    endpoint update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://school-website-full-stack.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
