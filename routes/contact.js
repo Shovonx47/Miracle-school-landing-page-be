3761530a326b9124b766f069f4cc9dddea2765bb
Shovonx47 <shovon@lamptechs.com>
2025-01-26 05:15:48 -0800

    Changes

--- routes/contact.js
+++ routes/contact.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const contactController = require('../controllers/contactController');
 
 // Route to get contact information
-router.get('/contact', contactController.getContactInformation);
+router.get('/', contactController.getContactInformation);
 
 module.exports = router;
