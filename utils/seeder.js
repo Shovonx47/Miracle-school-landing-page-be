e2d084ff7fc8aa352bcb72303b25426af9510962
Shovonx47 <shovon@lamptechs.com>
2025-01-26 22:24:22 -0800

    Notices API

--- utils/seeder.js
+++ utils/seeder.js
@@ -5,6 +5,7 @@ const AlbumImage = require('../models/albumImageModel');
 const News = require('../models/news');
 const administrationData = require('../data/administrationSeeder');
 const newsData = require('../data/news.json');
+const noticesData = require('../data/notices.json');
 
 // Function to seed administration data (governing body and album images)
 const seedAdministrationData = async () => {
@@ -54,6 +55,27 @@ const seedNewsData = async () => {
   }
 };
 
+// Function to seed notices data
+const seedNoticesData = async () => {
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB Connected...');
+
+    // Delete existing notices data
+    await News.deleteMany({ category: 'Notice' });
+    console.log('Existing notices data deleted');
+
+    // Insert new notices data
+    await News.insertMany(noticesData);
+    console.log('Notices data seeded successfully');
+    process.exit();
+  } catch (error) {
+    console.error('Error seeding notices data:', error);
+    process.exit(1);
+  }
+};
+
 // Check command line arguments
 const [,, command, modelName] = process.argv;
 
@@ -65,11 +87,14 @@ if (command === 'import') {
     case 'news':
       seedNewsData();
       break;
+    case 'notices':
+      seedNoticesData();
+      break;
     default:
-      console.error('Please use: node utils/seeder.js import [administration|news]');
+      console.error('Please use: node utils/seeder.js import [administration|news|notices]');
       process.exit(1);
   }
 } else {
-  console.error('Please use: node utils/seeder.js import [administration|news]');
+  console.error('Please use: node utils/seeder.js import [administration|news|notices]');
   process.exit(1);
 }
